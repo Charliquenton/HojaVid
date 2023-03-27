7a9e21bba685e9ff7368013298876df0b53abf3c
CarlosAndres102003 <221249@ids.upchiapas.edu.mx>
2023-03-26 21:10:42 -0600

    tercer commit

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,6 @@ git commit -m 'deploy'
 
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Charliquenton/HojaVid.git main:gh-pages
+# git push -f git@github.com:Charliquenton/HojaVid.git main:gh-pages
 
 cd -
