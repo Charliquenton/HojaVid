f147d1eee019842e4508113e67ce8a1c803d92c1
CarlosAndres102003 <221249@ids.upchiapas.edu.mx>
2023-03-26 20:29:01 -0600

    Initial commit

---
+++ src/Components/molecules/HojaVida.jsx
@@ -0,0 +1,28 @@
+
+
+function HojaVida() {
+    return ( 
+        <div className="div-father">
+
+            <div className="colum1">
+                <div>
+                    <img src="" alt="" />
+                </div>
+
+                <div>
+                    <h1>contactos</h1>
+                    <p>Telefono: 9681310329</p>
+                    <p>Facebook: Andresmartinez</p>
+                    <p>Correo: cmartinezmorales@gmail.com</p>
+                    
+                </div>
+            </div>
+
+            <div className="colum2"></div>
+
+
+        </div>
+     );
+}
+
+export default HojaVida;
